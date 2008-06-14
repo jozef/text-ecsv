b5006041ea6535a6a8fdf5544cdd57a2fb81b028
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2008-06-14 16:31:56 +0000

    initial Text-ECSV
    git-svn-id: https://cle.sk/repos/pub/cpan/Text-ECSV/trunk@410 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

---
+++ lib/Text/ECSV.pm
@@ -0,0 +1,107 @@
+package Text::ECSV;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Text::ECSV - The great new Text::ECSV!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Text::ECSV;
+
+    my $foo = Text::ECSV->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jozef Kutej, C<< <jozef at kutej.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-text-ecsv at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text-ECSV>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Text::ECSV
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Text-ECSV>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Text-ECSV>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Text-ECSV>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Text-ECSV>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Jozef Kutej, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Text::ECSV
